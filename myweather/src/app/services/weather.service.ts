0d3796652587f0b8ca195830d07228e5a38aa89a
alexbusbee <alex.busbee@gmail.com>
2017-01-09 11:57:56 -0600

    Added search bar

--- myweather/src/app/services/weather.service.ts
+++ myweather/src/app/services/weather.service.ts
@@ -7,15 +7,22 @@ export class WeatherService {
     http: any;
     apiKey: string;
     conditionsUrl: string;
+    searchUrl: string;
 
     constructor(http: Http) {
         this.http = http;
         this.apiKey = '44d5111015f19aa6';
-        this.conditionsUrl = 'http://api.wunderground.com/api/' + this.apiKey + '/conditions/q'
+        this.conditionsUrl = 'http://api.wunderground.com/api/' + this.apiKey + '/conditions/q';
+        this.searchUrl = 'http://localhost:8100/search/aq?query=';
     }
 
     getWeather(city, state) {
         return this.http.get(this.conditionsUrl + '/' + state + '/' + city + '.json')
+            .map(res => res.json());
+    }
+
+    searchCities(searchStr) {
+        return this.http.get(this.searchUrl + searchStr)
             .map(res => res.json());
     }
 }
--- myweather/src/pages/weather/weather.ts
+++ myweather/src/pages/weather/weather.ts
@@ -10,16 +10,25 @@ export class WeatherPage {
   city: string;
   state: string;
   weather: any;
+  searchStr: string;
+  results: any;
 
   constructor(public navCtrl: NavController, private weatherService: WeatherService) {
-    this.city = "Boston";
-    this.state = "MA";
+    this.city = "Milwaukee";
+    this.state = "WI";
   }
 
   ngOnInit() {
     this.weatherService.getWeather(this.city, this.state)
       .subscribe(weather => {
         this.weather = weather.current_observation;
+      });
+  }
+
+  getQuery() {
+    this.weatherService.searchCities(this.searchStr)
+      .subscribe(res => {
+        this.results = res.RESULTS;
       });
   }
 
